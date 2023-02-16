58e6dcb24c28dea62a930816abcd939631717201
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2023-02-17 00:01:38 +0530

    Fix: app layout fix

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,19 +2,24 @@
 
 import "../../styles/globals.scss";
 import React from "react";
+import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
+ import { getServerSession } from "next-auth";
 import Layout from "../components/Layout";
 
 interface IProps {
   children: ReactNode;
 }
-export default function RootLayout({ children }: IProps) {
+export default function RootLayout({ children }: IProps) {  
+  
   return (
     <html lang="en">
       <body>
-        <Layout>{children}</Layout>
-        <ToastContainer position="bottom-right" />
+        <SessionProvider>
+          <Layout>{children}</Layout>
+          <ToastContainer position="bottom-right" />
+        </SessionProvider>
       </body>
     </html>
   );
