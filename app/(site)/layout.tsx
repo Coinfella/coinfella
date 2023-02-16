6f24593bbbe26dce3c6cce5377e4a2a871327c71
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2023-02-17 00:09:28 +0530

    Fix: fixed error

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,19 +5,17 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
- import { getServerSession } from "next-auth";
-import Layout from "../components/Layout";
+import { getServerSession } from "next-auth";
 
 interface IProps {
   children: ReactNode;
 }
-export default function RootLayout({ children }: IProps) {  
-  
+export default function RootLayout({ children }: IProps) {
   return (
     <html lang="en">
       <body>
         <SessionProvider>
-          <Layout>{children}</Layout>
+          {children}
           <ToastContainer position="bottom-right" />
         </SessionProvider>
       </body>
