aea218d3ac956bafb1d2c2d9ae9ffba0ad96bdb9
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2023-02-17 00:08:50 +0530

    Fix: refactored layout

--- app/(landing_page)/layout.tsx
+++ app/(landing_page)/layout.tsx
@@ -4,7 +4,9 @@ import "../../styles/globals.scss";
 import React from "react";
 import { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
-import Layout from "../components/Layout";
+import Head from "next/head";
+import { Navbar } from "../components/Navbar";
+import Footer from "../components/Footer";
 
 interface IProps {
   children: ReactNode;
@@ -13,7 +15,17 @@ export default function RootLayout({ children }: IProps) {
   return (
     <html lang="en">
       <body>
-        <Layout>{children}</Layout>
+        <div className="h-screen">
+          <Head>
+            <title>CoinFella</title>
+            <meta name="description" content="Pay in fiat, receive crypto." />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+        </div>
         <ToastContainer position="bottom-right" />
       </body>
     </html>
